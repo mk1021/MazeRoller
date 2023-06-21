43b4ac416fc4d61ec02ee02450015b1b9e53f8de
Pierce <77743782+lemon-gith@users.noreply.github.com>
2023-06-21 14:41:44 +0100

    add control system code, draft 1

--- control_system/control_code.ino
+++ control_system/control_code.ino
@@ -13,7 +13,6 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <math.h>
-#include <Wire.h>
 
 Adafruit_MPU6050 mpu;
 
@@ -34,6 +33,7 @@ struct sensor_offsets{
 } stat_vals{};
 
 sensors_event_t a, g, temp;
+
 unsigned int prevTime, curTime;
 long double time_diff = 0;
 void updateTimeDiff(){
@@ -43,6 +43,7 @@ void updateTimeDiff(){
 }
 
 long double roll_ang, pitch_ang, yaw_ang;
+long double prev_yaw_ang;
 
 void calibrate_sensors(int offset_loops){
   Serial.print("Calibrating sensors (");
@@ -169,13 +170,15 @@ void sensor_update(){
 
   if(pitch_ang != pitch_ang) pitch_ang = 0;
 
+  prev_yaw_ang = yaw_ang;
   yaw_ang += yaw_vel*time_diff;
 }
 
 
 long double prev_ang_err, curr_ang_err, total_ang_err;
 const long double K_p = 1, K_i = 1, K_d = 1; // TODO: change this to make it do the balance
-const double angle_to_mvmnt = 3.1415; // TODO: change this to make output fit desired movement change
+const double control_to_mvmnt = 3.1415; // TODO: change this to make output fit desired movement change
+// a higher value means more movement to achieve a desired pitch, lower value means less movement
 
 // Maybe think about acceleration during movement, how much does that affect the correction?
 
@@ -186,33 +189,108 @@ int pitch_control(long double desired_pitch_ang){
   pitch_control_output +=  K_p*curr_ang_err;
   pitch_control_output +=  K_i*(total_ang_err += curr_ang_err*time_diff);
   pitch_control_output +=  K_d*(curr_ang_err - prev_ang_err)/time_diff;
-  // pitch_control_output += K_x*x_position(or x_theta);
+  // pitch_control_output += K_x*x_position(or x_theta);  //  TODO: decide if this is even necessary
+  //  ^^ was supposed to reduce speed the closer you got to your desired position, but idk
 
   prev_ang_err = curr_ang_err;
-  return (int)(pitch_control_output*angle_to_mvmnt);
+  return (int)(pitch_control_output*control_to_mvmnt);
 }
 
 int x_position = 0;
 int x_theta = 0;
 int x_moved = 0;
+int z_to_rotate = 0; // positive is clockwise, negative is anti-clockwise
+int movement_input = 0; // from exploration, will be used to adjust x_position
+bool rotation = false;
+const double yaw_to_rotation = 60; // TODO: tune to get yaw to rotation amount
+const double pos_to_ang = 5; // TODO: tune to give better value for desired pitch angle
+// i.e. if it's tilting too much and falling over when moving, this needs to be higher,
+//      if it's not tilting enough and falling backwards when moving, this needs to be lower, etc.
 
+// outputs the desired pitch angle, to set the rover up for motion
 long double position_control(){
-  if (x_position == 0) return 0;
-  
-  return x_position/5;
+  return atan((x_position + x_theta)/pos_to_ang); // should return 0 for x_position == 0
 }
 
-bool dynamic = false;
+// sign of x_theta and x_position should give the direction of acceleration,
+// with ==0 describing whether the motors were even on or not
+short sign_of(int *operand, bool approximate_zero=false){
+    if(approximate_zero){
+      if(abs(*operand) < 3) return 0;
+    }
+    else{
+      if(*operand == 0) return 0;
+    }
+
+    if(*operand < 0) return -1;
+    else return 1;
+}
+
+bool dynamic = false;   // variable holding current state
+double linear_velocity = 0;
+/*const*/ double approx_acceleration_magnitude = 2; // TODO: find value for rough rate of motors accelerating the rover
+
+// should just be used in below functions, only sign changes
+inline double approx_accel(){
+    return approx_acceleration_magnitude * (dynamic ? sign_of(x_position) : sign_of(x_theta));
+}
+
+// must be called before update_velocity() in loop()
+inline int distance_moved(){
+    return (int)(linear_velocity*time_diff + 0.5*approx_accel()*sq(time_diff));
+}
+
+// v = u + at
+double update_velocity(){
+   linear_velocity += approx_accel()*time_diff;
+}
+
+// ------------------ simple movement functions -----------------------
+
+void move_forward(){
+  digitalWrite(dirPinL, HIGH);
+  digitalWrite(dirPinR, HIGH);
+
+  digitalWrite(stepPinL, HIGH);
+  digitalWrite(stepPinR, HIGH);
+}
+
+void move_backward(){
+  digitalWrite(dirPinL, LOW);
+  digitalWrite(dirPinR, LOW);
+
+  digitalWrite(stepPinL, HIGH);
+  digitalWrite(stepPinR, HIGH);
+}
+
+void move_clockwise(){
+  digitalWrite(dirPinL, HIGH);
+  digitalWrite(dirPinR, LOW);
+
+  digitalWrite(stepPinL, HIGH);
+  digitalWrite(stepPinR, HIGH);
+}
+
+void move_anticlockwise(){
+  digitalWrite(dirPinL, LOW);
+  digitalWrite(dirPinR, HIGH);
+
+  digitalWrite(stepPinL, HIGH);
+  digitalWrite(stepPinR, HIGH);
+}
+
+void move_stationary(){
+  digitalWrite(stepPinL, LOW);
+  digitalWrite(stepPinR, LOW);
+}
+
+
+// ------------------ main loop -----------------------
 
 void loop() {
   comm = Serial.readString();
-
-  if (comm == "LF") digitalWrite(dirPinL, HIGH);
-  else if (comm == "LB") digitalWrite(dirPinL, LOW);
-  else if (comm == "RF") digitalWrite(dirPinR, HIGH);
-  else if (comm == "RB") digitalWrite(dirPinR, LOW);
   
-  else if (comm == "C10") calibrate_sensors(10);
+  /*else */if (comm == "C10") calibrate_sensors(10);
   else if (comm == "C50") calibrate_sensors(50);
   
   else if (comm == ""){}
@@ -225,50 +303,72 @@ void loop() {
     Serial.println(comm);
   }
 
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  /*//Spin the stepper motor 5 (before the loop, so 25 now?) revolutions fast:
-  // These four lines result in 1 step:
-  for(int i = 0; i < 5; i++){
-    digitalWrite(stepPinL, HIGH);
-    digitalWrite(stepPinR, HIGH);
-    delayMicroseconds(2000);
-
-    digitalWrite(stepPinL, LOW);
-    digitalWrite(stepPinR, LOW);
-    delayMicroseconds(2000);
-  }*/
-
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
   sensor_update();
 
-  // if (x_theta == 0) x_position -= x_moved;  // to be corrected on later loops, idk
+  // ------------------------------------    Control System     ---------------------------------------------
+
+  x_moved = distance_moved(); // I'm assuming the x values are measured in steps or sth, so they'll be in the hundreds?
+  update_velocity();
   
-  if (dynamic){
-    ; // TODO: to be implemented
+  if(dynamic){  // shouldn't be interrupted while in dynamic mode, since it's supposed to be tipping
+    x_position -= x_moved;
+    switch (sign_of(x_position, approximate_zero=true){
+      case 1:
+        move_forward();
+        break;
+      case -1:
+        move_backward();
+        break;
+      default:
+        x_position = 0;
+        dynamic = false;
+        move_stationary();
+        break;
+    }
+
+    // TODO: tune these three values, which as killswitches for dynamic mode
+    if((abs(pitch_ang) < 0.01)  // not sure about this condition, I feel if it's upright before reaching it's target,
+                                // it's probably gonna fall back, but I'm not sure
+    || (abs(pitch_vel) > 5)     // if it's falling too quickly, probably want to stop?
+    || (abs(linear_velocity) > 100))  // if it's moving too quickly, that's also probably not good
+        dynamic = false; // turn off dynamic mode
   }
   else{ // for the static case
+    x_theta -= x_moved; // x_theta is used in position_control()
     x_theta = pitch_control(position_control());
-    if (abs(x_theta) < 5){
-      digitalWrite(stepPinL, LOW);
-      digitalWrite(stepPinR, LOW);
+
+    if (abs(x_theta) < 2){ // is balanced
+      if(x_position == 0){ // has nowhere left to travel to
+        z_to_rotate -= (yaw_ang - prev_yaw_ang) * yaw_to_rotation;
+        if(z_to_rotate == 0){ // has nowhere left to rotate to
+          // deal with exploration inputs
+          if(movement_input != 0){
+            if(rotation) z_to_rotate += movement_input;
+            else x_position += movement_input;
+            movement_input = 0;
+          }
+          move_stationary();
+        }
+        else{ // has yet to rotate, rotation isn't the same as dynamic, can only take place when tilt is correct
+          switch (sign_of(z_to_rotate, approximate_zero=true){
+            case 1:
+              move_clockwise();
+              break;
+            case -1:
+              move_anticlockwise();
+              break;
+            default:
+              z_to_rotate = 0;
+              move_stationary();
+              break;
+          }
+        }
+      }
+      else dynamic = true;
     }
     else{ 
-      if(x_theta > 0){
-        digitalWrite(dirPinL, HIGH);
-        digitalWrite(dirPinR, HIGH);
-      }
-      else{
-        digitalWrite(dirPinL, LOW);
-        digitalWrite(dirPinR, LOW);
-      }
-      digitalWrite(stepPinL, HIGH);
-      digitalWrite(stepPinR, HIGH);
+      if(x_theta > 0) move_forward();
+      else move_backward();
     }
   }
-  
-  /* // Movement Controller
-     // use x_theta to move rover by a certain amount
-  */
 }
