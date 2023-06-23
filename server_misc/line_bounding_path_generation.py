55c060b1dc66f60f7769154afce3e2e97d1cdef2
pierce <77743782+lemon-gith@users.noreply.github.com>
2023-06-23 02:48:39 +0100

    get started designing the function

--- server_misc/line_bounding_path_generation.py
+++ server_misc/line_bounding_path_generation.py
@@ -1,5 +1,5 @@
 import math
-
+from main_script import DataEntry
 
 class Coords:
     """only a single pair of values, but to be paired up with another pair :D"""
@@ -10,22 +10,38 @@ class Coords:
         self.x, self.y = x_coord, y_coord
 
 
-class CoordPairs:
-    """a pair of a pair of values, to be paired used with another pair, in pair :)"""
-    L: float
+class CoordPair:
+    """a pair of a pair of values, to be paired and used with another pair, in a pair :)"""
+    L: Coords
     R: Coords
 
     def absolute_value(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def __init__(self, a: float = 0, b: float = 0):
-        self.x, self.y = a, b
+    def __init__(self, left_coords: Coords, right_coords: Coords):
+        if not left_coords or not right_coords:
+            raise ValueError("One of these coordinates is NULL")
+        self.L = left_coords
+        self.R = right_coords
+
+
+class Quadrilateral:
+    """The Quads, this is the class that does the heavy-lifting"""
+    Top: CoordPair
+    Bottom: CoordPair
+
+    def absolute_value(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def __init__(self, left_coords: Coords, right_coords: Coords):
+        if not left_coords or not right_coords:
+            raise ValueError("One of these coordinates is NULL")
+        self.L = left_coords    # TODO: implement this class
+        self.R = right_coords
 
 
 def generate_path(coordinates):
-    prev_heading = 0
-    coord = [0, 0]
-    print("hi")
+
 
 
 if __name__ == '__main__':
